7423d351adbed2b34f530277c0ebf42d9739c89c
Frik-dev <140255028+Frik-dev@users.noreply.github.com>
2024-07-09 13:21:55 +0200

    PYTHON BASIC AMENDMENT

---
+++ BASICS/INPUT.py
@@ -0,0 +1,10 @@
+Name = input("What is your name?")
+print('your name is ' + Name)  # WHEN YOU COMBINE STRING AND INTEGER IT'S CALLED CONCATENATION
+
+# EXERCISE: Write a program to ask a user his/her name and his/her favorite color and print the inputs
+
+# SOLUTION
+
+input("What's your name? ")
+input("What's your favorite color? ")
+
---
+++ BASICS/VARIABLES.py
@@ -0,0 +1,20 @@
+# VARIABLE IS USED TO LABEL SOMETHING THAT NEEDS TO BE STORED
+
+# WHEN YOU ARE STORING A WORD OR STRING YOU USE DOUBLE QUOTES AND IT DOES NOT APPLY ON A NUMBER
+Name = 'Ishimwe Christian'  # 'STRING' Ishimwe Christian is assigned to a variable 'Name'
+Age = 10  # 'INTEGER' 10 is assigned to a variable 'Age'
+IsMale = True  # 'BOOLEAN' True is assigned to a variable 'IsMale'
+Weight = 50.2  # 'FLOAT' 50.2 is assigned to a variable 'Weight'
+
+print(Age)
+print(Name)
+print(IsMale)
+print(Weight)
+
+# EXCERCISE: Write a program to indicate a person called Frikist and he's 20years old and he has 50.3kgs
+
+# SOLUTION
+
+Name = "Frikist"
+Age = 20
+Weight = 50.3
